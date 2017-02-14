9088f31dba5350caa04c4886ca7ee9ad38531518
fuqiang <fuqiang1@hoolai.com>
2017-02-14 12:35:09 +0800

    feat(app.vue) 添加common.css

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <div> 调用jq</div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+// import thirdJs from 'js/jquery.min.js';
 export default {
   name: 'app'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css">
+  @import 'css/common.css';
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    font-size: 16px;
+  }
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: '你好你好你哈哈的护肤环节'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
