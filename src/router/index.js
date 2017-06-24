0d8555c69233b6d5abe477d611ca0d76db56e0bb
fuqiang <fuqiang@hoolai.com>
2017-06-24 18:03:44 +0800

    fix(index.js} 引入的模块和路由配置顺序要一致

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../components/Home.vue'
 import UserCenter from '../components/user-center/UserCenter.vue'
-import HelpCenter from '../components/activity/HelpCenter.vue'
 import ProjectDetail from '../components/ProjectDetail.vue'
 import Invite from '../components/activity/invite.vue'
 import ActivityReward from '../components/activity/reward.vue'
-// import Questionnaire from '../components/user-center/Questionnaire.vue'
+import HelpCenter from '../components/user-center/HelpCenter.vue'
+import Questionnaire from '../components/user-center/Questionnaire.vue'
 Vue.use(Router)
 const routes = [
   {
@@ -38,6 +38,11 @@ const routes = [
     path: '/user-center/help-center',
     name: 'HelpCenter',
     component: HelpCenter
+  },
+  {
+    path: '/user-center/questionnaire',
+    name: 'Questionnaire',
+    component: Questionnaire
   }
 ]
 const router = new Router({
