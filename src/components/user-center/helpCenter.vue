c5a6cd62b7319ec4a9ede3295012917438e7d7cc
fuqiang <fuqiang@hoolai.com>
2017-07-05 17:09:39 +0800

    feat(helpCenter.vue) 帮助中心页面

--- src/components/user-center/helpCenter.vue
+++ src/components/user-center/helpCenter.vue
@@ -1,28 +1,68 @@
 <template>
   <div class="help-center">
     <router-link :to="{name: 'Help',params: {type: 1}}">
-      <div>了解宏财</div>
+      <div class="class1">
+        <img src="../../images/user-center/ljhc.png" alt="" class="display-bl margin-auto" width="100%">
+        <div class="mask">
+          <span class="line1 display-inbl"></span><span>了解宏财</span><span class="line2 display-inbl"></span>
+        </div>
+      </div>
     </router-link>
     <router-link :to="{name: 'Help',params: {type: 2}}">
-      <div>产品介绍</div>
+      <div class="class1">
+        <img src="../../images/user-center/cpjs.png" alt="" class="display-bl margin-auto" width="100%">
+        <div class="mask">
+          <span class="line1 display-inbl"></span><span>产品介绍</span><span class="line2 display-inbl"></span>
+        </div>
+      </div>
     </router-link>
     <router-link :to="{name: 'Help',params: {type: 3}}">
-      <div>投资相关</div>
+      <div class="class1">
+        <img src="../../images/user-center/tzxg.png" alt="" class="display-bl margin-auto" width="100%">
+        <div class="mask">
+          <span class="line1 display-inbl"></span><span>投资相关</span><span class="line2 display-inbl"></span>
+        </div>
+      </div>
     </router-link>
     <router-link :to="{name: 'Help',params: {type: 4}}">
-      <div>注册登录</div>
+      <div class="class1">
+        <img src="../../images/user-center/dlzc.png" alt="" class="display-bl margin-auto" width="100%">
+        <div class="mask">
+          <span class="line1 display-inbl"></span><span>注册登录</span><span class="line2 display-inbl"></span>
+        </div>
+      </div>
     </router-link>
     <router-link :to="{name: 'Help',params: {type: 5}}">
-      <div>账户管理</div>
+      <div class="class1">
+        <img src="../../images/user-center/dlzc.png" alt="" class="display-bl margin-auto" width="100%">
+        <div class="mask">
+          <span class="line1 display-inbl"></span><span>账户管理</span><span class="line2 display-inbl"></span>
+        </div>
+      </div>
     </router-link>
     <router-link :to="{name: 'Help',params: {type: 6}}">
-      <div>充值提现</div>
+      <div class="class1">
+        <img src="../../images/user-center/cztx.png" alt="" class="display-bl margin-auto" width="100%">
+        <div class="mask">
+          <span class="line1 display-inbl"></span><span>充值提现</span><span class="line2 display-inbl"></span>
+        </div>
+      </div>
     </router-link>
     <router-link :to="{name: 'Help',params: {type: 7}}">
-      <div>用户福利</div>
+      <div class="class1">
+        <img src="../../images/user-center/uhfl.png" alt="" class="display-bl margin-auto" width="100%">
+        <div class="mask">
+          <span class="line1 display-inbl"></span><span>用户福利</span><span class="line2 display-inbl"></span>
+        </div>
+      </div>
     </router-link>
     <router-link :to="{name: 'Help',params: {type: 8}}">
-      <div>客户服务</div>
+      <div class="class1">
+        <img src="../../images/user-center/khfw.png" alt="" class="display-bl margin-auto" width="100%">
+        <div class="mask">
+          <span class="line1 display-inbl"></span><span>客户服务</span><span class="line2 display-inbl"></span>
+        </div>
+      </div>
     </router-link>
   </div>
 </template>
@@ -40,36 +80,45 @@
 </script>
 
 <style scoped>
-  .help-center .content {
-    overflow: hidden;
-    padding: 0 .36rem;
+  .help-center {
     background-color: #fff;
+    font-size: .26rem;
+    padding: .4rem;
   }
-  .help-center .content.open {
-    background-color: #f8f9fd !important;
-    box-sizing: border-box;
+  img {
+    border-radius: .2rem;
   }
-  .help-center .content.open .column {
-    background-color: #f8f9fd !important;
-    border: none!important;
-  }
-  .help-center .content .column {
-    border-bottom: 1px solid #eee;
-    padding: .2rem 0rem;
-    background-color: #fff;
-    margin-left: 0;
+  .class1, .class2, .class3, .class4, .class5, .class6, .class7, .class8 {
+    margin-bottom: .3rem; 
     width: 100%;
-    float: left;
-    color: #666;
+    position: relative;
+    line-height: 2.2rem;
+    color: #fff;
   }
-  .column .rotate {
-    transform: rotateZ(90deg);
+  .line1, .line2 {
+    display: inline-block;
+    width: 20%;
+    height: 1px;
+    background-color: #fff;
+    margin-bottom: 0.08rem;
   }
-  .submenu {
-    display: none;
-    text-align: justify;
-    padding-left: .2rem;
-    padding-bottom: .25rem;
-    color: #999;
+  .line1 {
+    margin-right: .2rem;
+  }
+  .line2 {
+    margin-left: .2rem;
+  }
+  .class8 {
+    margin-bottom: .5rem;
+  }
+  .mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: rgba(0,0,0,.75);
+    border-radius: .2rem;
+    z-index: 999;
   }
 </style> 
