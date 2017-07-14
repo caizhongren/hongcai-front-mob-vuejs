09c4d7ae6ef381d9cee274939e0fb25cba7f23c5
yuyang <yuyang1@hooali.com>
2017-07-14 17:17:43 +0800

    fix(assignmentList.vue):转让记录 分页

--- src/components/user-center/assignmentList.vue
+++ src/components/user-center/assignmentList.vue
@@ -5,18 +5,18 @@
       <li>转让本金(元)</li>
       <li>转让时间</li>
     </ul>
-    <div class="no-record" v-if="assignmentOrder.length <= 0">
-      <img src="../../images/project/no-record.png" width="35%" class="no-record">
-      <p class="ft-grey4 margin-b-0 margin-t-1p5">暂无记录</p>
-    </div>
-    <ul class="list" v-else="assignmentOrder.length > 0">
+    <ul class="list" v-if="assignmentOrder && assignmentOrder.length > 0">
       <li v-for="order in assignmentOrder">
-        <span>{{order.userName}}</span>
+        <span>{{order.userName}}{{assignmentOrderList.length}}</span>
         <span>{{order.orderAmount}}</span>
         <span>{{order.createTime | date}}</span>
       </li>
-      <div class="loadMuch text-center" @click="loadMore">查看更多</p></div>
+      <div class="loadMuch text-center" @click="loadMore(limit)" v-if="assignmentOrderList.length > limit">查看更多</p></div>
     </ul>
+    <div class="no-record" v-else="assignmentOrder && assignmentOrder.length <= 0">
+      <img src="../../images/project/no-record.png" width="35%" class="no-record">
+      <p class="ft-grey4 margin-b-0 margin-t-1p5">暂无记录</p>
+    </div>
     <img class="common-bg" src="../../images/common-bg.png" alt="">
   </div>
 </template>
@@ -26,37 +26,34 @@
     name: 'assignmentList',
     data () {
       return {
-        assignmentOrder: [
-          // {userName: '1', orderAmount: 1, createTime: 1500007748123},
-          // {userName: '2', orderAmount: 2, createTime: 1500007748123},
-          // {userName: '3', orderAmount: 3, createTime: 1500007748123}
-        ]
+        assignmentOrderList: [],
+        assignmentOrder: [],
+        limit: 10,
+        token: ''
       }
     },
     created: function () {
       this.assignmentNumber = this.$route.params.number
-      this.getAssignmentOrder()
+      this.token = this.$route.query.token
+      this.getAssignmentOrder(this.limit)
     },
     methods: {
-      getAssignmentOrder: function () {
+      getAssignmentOrder: function (limit) {
         var that = this
         that.$http({
           method: 'get',
-          url: '/hongcai/rest/assignments/' + that.assignmentNumber + '/orders?token=6261f5e1e9eb93e9479f8cf19c1b2e986ab535d7a0e01c51&page=1&pageSize=2'
+          url: '/hongcai/rest/assignments/' + that.assignmentNumber + '/orders?token=' + that.token
         }).then(function (res) {
           if (res.data && res.data.ret !== -1) {
-            that.assignmentOrder = res.data.data
-            console.log(that.assignmentOrder)
+            that.assignmentOrderList = res.data.data
+            that.assignmentOrder = that.assignmentOrderList.slice(0, limit)
+            // console.log(that.assignmentOrder)
           }
         })
       },
-      loadMore: function () {
-        alert(11)
-      }
-    },
-    filters: {
-      limit: function (arr, limit) {
-        return arr.slice(0, parseInt(limit))
+      loadMore: function (limit) {
+        this.limit = limit + 1
+        this.assignmentOrder = this.assignmentOrderList.slice(0, this.limit)
       }
     }
   }
