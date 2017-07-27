da90e930ed0622631eaf574c4862762e5a177f76
fuqiang <fuqiang@hoolai.com>
2017-07-27 10:46:38 +0800

    fix(发现) 添加发现文件夹

---
+++ src/components/find/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        msg: '新闻页1'
+      }
+    }
+  }
+</script>
+<style scoped>
+
+</style>
