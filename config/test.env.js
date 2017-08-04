6613d655736285d21d29dc08622f9e49eed7ad43
zhangfeng <zhangfeng@hoolai.com>
2017-08-04 17:54:26 +0800

    fix(config): test config

--- config/test.env.js
+++ config/test.env.js
@@ -2,5 +2,7 @@ var merge = require('webpack-merge')
 var devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"'
+  NODE_ENV: '"testing"',
+  baseFileUrl: '"http://test321.hongcai.com/uploads/"',
+  domain: '"http://m.test321.hongcai.com"'
 })
