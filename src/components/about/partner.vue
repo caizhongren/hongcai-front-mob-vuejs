785045771688f99f6cce01c31e0f9ae1670c10d0
yuyang <yuyang1@hooali.com>
2017-08-10 16:50:41 +0800

    fix(partner.vue):更换2倍图

--- src/components/about/partner.vue
+++ src/components/about/partner.vue
@@ -1,35 +1,35 @@
 <template>
   <div class="partner">
     <div class="part">
-      <img src="../../images/about/partner-1.png" alt="">
+      <img src="../../images/about/partner-1.png" alt="" width="60%">
       <p>宏财网项目推荐方为鑫融基金控股份有限公司（以下简称鑫融基，证券代码：832379）。鑫融基注册资本16.39亿元，是2006年由政府出资发起，多家地方企业共同参与成立的金融机构。是新三板金控第一股和首家民间全牌照金融服务机构，股本总额位居全国第四。</p>
     </div>
     <div class="part">
-      <img src="../../images/about/partner-2.png" alt="">
+      <img src="../../images/about/partner-2.png" alt="" width="24%">
       <p>国信卓越信息咨询有限公司（以下简称国信）为有信伟业集团主要控股，是有信金融四驾马车之一。国信始终坚持“以小为美”，专注为中小微企业提供专业、便捷、多元化的金融服务。</p>
     </div>
     <div class="part">
-      <img src="../../images/about/partner-3.png" alt="">
+      <img src="../../images/about/partner-3.png" alt="" width="42%">
       <p>北京青桐资产管理有限公司，注册资本金1000万元人民币，青桐资产以“金融为主，产业为辅、产融结合”为经营理念，业务涵盖汽车金融和房产金融。</p>
     </div>
     <div class="part">
-      <img src="../../images/about/partner-4.png" alt="">
-      <p>宏财网已正式对接海口联合农商银行资金存管系统。标的、资金信息同步上传北京市网贷行业协会，最大限度保护投资者利益。</p>
+      <img src="../../images/about/partner-4.png" alt="" width="44%">
+      <p>宏财网已正式对接海口联合农商银行资金存管系统（简称银行资金存管系统）。标的、资金信息同步上传北京市网贷协会，最大限度保护投资者利益。</p>
     </div>
     <div class="part">
-      <img src="../../images/about/partner-5.png" alt="">
-      <p>富友金融自2008年创立至今，已经发展成为一家大型的金融综合服务集团公司，注册资金2.0007亿元，实收资本金3.8亿元，下设6加全资子公司和1家控股子公司，同时拥有6家支付及金融服务牌照，并在全国设立了35家分公司，立志成为互联网金融和网络生活服务的领跑者。</p>
+      <img src="../../images/about/partner-5.png" alt="" width="24%">
+      <p>易宝支付（YeePay.com)成立于2003年8月成立，总部位于北京，在全国设有30家分公司，是中国行业支付的开创者和领导者。自公司成立以来，秉承成就客户、极致服务、开放分享的核心价值观，致力成为世界一流的交易服务平台，领跑电子支付、移动互联和互联网金融。</p>
     </div>
     <div class="part">
-      <img src="../../images/about/partner-6.png" alt="">
+      <img src="../../images/about/partner-6.png" alt="" width="37%">
       <p>联动优势科技有限公司是一家专业提供金融科技创新服务的互联网高新技术企业，在2016中国互联网百强企业名录中位列第36位，是中国互联网金融协会理事单位、中国支付清算协会常务理事单位。</p>
     </div>
     <div class="part">
-      <img src="../../images/about/partner-7.png" alt="">
+      <img src="../../images/about/partner-7.png" alt="" width="28%">
       <p>深圳市嘉得投资基金管理有限公司注册资本2亿元，主营业务为资产管理业务、股权和证券投资业务、管理咨询服务等。公司成立至今，资产管理规模从人民币1000万发展到人民币数十亿元左右，平均回报率超过50%，投研人员平均从业经验10年以上。</p>
     </div>
     <div class="part">
-      <img src="../../images/about/partner-8.png" alt="">
+      <img src="../../images/about/partner-8.png" alt="" width="30%">
       <p>宏财网与工信部下设中国中小企业信息网（www.sme.gov.cn）达成战略合作协议。成为首个与中小企业信息网合作的互联网金融平台。</p>
     </div>
   </div>
