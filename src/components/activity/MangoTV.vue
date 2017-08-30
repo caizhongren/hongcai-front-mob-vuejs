50e8de5e323d873983f8357145f51c620eee5b92
yuyang <yuyang1@hooali.com>
2017-08-30 17:16:52 +0800

    fix(mangotv):芒果tv更改卡片

--- src/components/activity/MangoTV.vue
+++ src/components/activity/MangoTV.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="Mango" v-bind:class="{ 'padding-b-5': !isIos }">
+  <div class="Mango" v-bind:class="{ 'padding-b-5': !isIos }" v-if="token">
     <div class="head-img">
       <div class="head-txt">携手宏财网 &nbsp;&nbsp;&nbsp; 开启新视界</div>
     </div>
-    <div class="boxes">
+    <!-- 礼遇1\2 -->
+    <div class="courtesies">
+      <!-- 礼遇1 -->
       <div class="courtesy1">
         <p class="courtesy-title">0元变身VIP，追星看剧更华丽</p>
       </div>
       <div class="courtesy1-content comm-bg">
         <div class="courtesy1-lf">
           <div class="pic"></div>
-          <div class="stock"><p>剩余：500份</p></div>
+          <div class="stock" v-show="actEnding === 1"><p>剩余：{{monthCount}}份</p></div>
         </div>
         <div class="courtesy1-rt">
-          <div class="txt" v-if="userAuth.active === true">您的<span class="ft-org">芒果TV会员1个月</span>奖励兑换码已自动发送至您的站内信提醒</div>
+          <div class="txt margin-b-1" v-if="userAuth.active === true">您的<span class="ft-org">芒果TV会员1个月</span>奖励兑换码已自动发送至您的站内信提醒</div>
           <div class="txt" v-if="userAuth.active === false">您已获得<span class="ft-org">芒果TV会员1个月</span>奖励资格，开通银行存管后，兑换码将自动发送至您的站内信提醒</div>
-          <div class="InvestBtn" @click="toMessage">{{userAuth.active === false ? '立即开通' : '查看兑换码'}}</div>
+          <div class="InvestBtn" @click="toCheckAuth">{{userAuth.active === false ? '立即开通' : '查看兑换码'}}</div>
         </div>
+        <!-- <div class="courtesy1-rt">
+          活动已结束
+        </div> -->
       </div>
+      <!-- 礼遇2 -->
       <div class="courtesy2">
         <p class="courtesy-title">首投VIP再升级，季卡年卡送给你</p>
       </div>
@@ -25,28 +31,31 @@
         <ul class="courtesy2-card">
           <li v-for="card in Member">
             <div class="card">
-              <p class="card-level">{{card.level}}</p>
-              <p class="card-limit">{{card.investLimit}}</p>
+              <p class="card-limit">首笔投资满{{card.minInvestAmount}}元</p>
             </div>
-            <p class="stock" v-show="!actEnding">{{card.amount < 0 ? '补货中' : '剩余：' + card.amount + '份'}}</p>
+            <p class="stock" v-show="actEnding === 1">{{card.countNum < 0 ? '补货中' : '剩余：' + card.countNum + '份'}}</p>
           </li>
-          <img src="../../images/mangoTV/act-ending.png" alt="" class="actEnd" v-show="actEnding">
+          <img src="../../images/mangoTV/act-ending.png" alt="" class="actEnd" v-show="actEnding === 2">
         </ul>
-        <div class="reward" v-show="!actEnding">
-          <img src="../../images/mangoTV/act-reward.png" alt="" width="65%">
+        <!-- 我的奖励 -->
+        <div class="reward" v-show="!noInvest">
+          <img src="../../images/mangoTV/act-reward.png" alt="" width="70%">
+          <!-- 未进行过投资 -->
           <div class="noInvest" v-show="noInvest">
             您还未进行过投资呢！<br>
             快去挑选好想要的奖励，
             <p class="tips-red">投资达标再来活动页兑换吧～</p>
           </div>
+          <!-- 投资为满足兑换条件 -->
           <div class="noInvest" v-show="InvestLimit">
             <p>您首次投资金额未满足以上奖励兑换条件，前往首页，还有更多精彩活动等您参与～</p>
           </div>
-          <div class="notExchange" v-show="notExchange">
-            年度VIP
+          <!-- 获得奖励未兑换 -->
+          <div class="notExchange reward-2" v-show="notExchange">
+            <!-- 奖励已兑换 -->
             <img src="../../images/mangoTV/act-exchange.png" alt="" class="exchange" v-show="hasExchange">
           </div>
-          <div class="InvestBtn">{{noInvest || InvestLimit ? '立即投资' : notExchange ? '立即兑换' : '查看兑换码'}}</div>
+          <div class="InvestBtn" @click="toExchange()" v-show="noInvest || notExchange || hasExchange">{{noInvest ? '立即投资' : notExchange && hasExchange ? '查看兑换码' : '立即兑换'}}</div>
         </div>
         <div class="hot-tips">
           *温馨提示: <br>
@@ -55,12 +64,13 @@
         </div>
       </div>
     </div>
+    <!-- 活动规则 -->
     <div>
       <div class="rule-head"></div>
       <div class="ruleBox comm-bg">
         <div class="rule">
           <span class="rule-no">1.</span>
-          <span class="rule-txt">活动奖励数量有限，先到先得，兑完即止，如活动结束后7天内还未兑换，则奖励实效；</span>
+          <span class="rule-txt">活动奖励数量有限，先到先得，兑完即止，如活动结束后7天内还未兑换，则奖励失效；</span>
         </div>
         <div class="rule">
           <span class="rule-no">2.</span>
@@ -68,13 +78,13 @@
         </div>
         <div class="rule">
           <span class="rule-no">3.</span>
-          <span class="rule-txt">礼遇一与礼遇二奖励可叠加获得，但礼遇二奖励仅可兑换一次，一定选好再兑换哟；</span>
+          <span class="rule-txt">礼遇一与礼遇二奖励可叠加获得，但礼遇二奖励仅可兑换一次，一定要看准兑换条件再进行投资哟；</span>
         </div>
         <div class="rule">
           <span class="rule-no">4.</span>
           <span class="rule-txt">如发现用户活动中涉及造假、作弊等嫌疑，平台有权利取消其获奖资格并冻结账号；</span>
         </div>
-        <div class="rule">
+        <div class="rule margin-b-0">
           <span class="rule-no">5.</span>
           <span class="rule-txt">在法律规定范围内，平台保留本活动最终解释权；</span>
         </div>
@@ -82,16 +92,22 @@
     </div>
     <div class="iosTip" v-show="isIos">该活动与设备生产商Apple Inc.公司无关</div>
     <!-- 领取成功弹窗 -->
-    <div class="dialog mask-common" v-if="false">
+    <div class="dialog mask-common" v-if="showMask">
       <div class="successBg">
-        <div v-if="false">
-          <img src="../../images/mangoTV/act-upperLimit.png" alt="" class="receive-title">
-          <p class="receive-cont">前往【我的】页面点击右上角，进入站内信【提醒】即可查看咯！</p>
-          <div class="IKnowBtn margin-t-1" @click="ShowReceive">我知道了</div>
+        <!-- 补货中 -->
+        <div v-if="upperLimit">
+          <div class="upperLimit">
+            <img src="../../images/mangoTV/act-upperLimit.png" alt="" class="receive-title">
+            <p class="receive-cont">该奖励今日已兑完！正在火速补货中，明天再来试试吧！</p>
+          </div>
+          <div class="IKnowBtn" @click="ShowReceive">我知道了</div>
         </div>
-        <div class="receive" v-if="true">
-          <img src="../../images/mangoTV/act-receive.png" alt="" class="receive-title">
-          <p class="receive-cont">前往【我的】页面点击右上角，进入站内信【提醒】即可查看咯！</p>
+        <!-- 查看兑换码 -->
+        <div v-if="receiveBox">
+          <div class="receive">
+            <img src="../../images/mangoTV/act-receive.png" alt="" class="receive-title">
+            <p class="receive-cont">前往【我的】页面点击右上角，进入站内信【提醒】即可查看咯！</p>
+          </div>
           <div class="btns">
             <div class="IKnowBtn fl" @click="ShowReceive">我知道了</div>
             <div class="toMessage fr" @click="toMessage">查看兑换码</div>
@@ -102,28 +118,27 @@
   </div>
 </template>
 <script>
-  import {Utils, bridgeUtil} from '../../service/Utils'
+  import {Utils, bridgeUtil, ModalHelper} from '../../service/Utils'
   export default {
     name: 'Mango',
     data () {
       return {
         Member: [
           {
-            level: '3个月VIP',
-            investLimit: '首笔投资满1000元',
-            amount: -1
-          }, {
-            level: '半年VIP',
-            investLimit: '首笔投资满2000元',
-            amount: 500
-          }, {
-            level: '年度VIP',
-            investLimit: '首笔投资满5000元',
-            amount: 5555
-          }, {
-            level: '年度全屏VIP',
-            investLimit: '首笔投资满10000元',
-            amount: 500
+            minInvestAmount: 100,
+            countNum: 100
+          },
+          {
+            minInvestAmount: 100,
+            countNum: 100
+          },
+          {
+            minInvestAmount: 100,
+            countNum: 100
+          },
+          {
+            minInvestAmount: 100,
+            countNum: 100
           }
         ],
         isIos: Utils.isIos(),
@@ -131,29 +146,86 @@
           active: false,
           authStatus: Number
         },
+        monthCount: 0,
         noInvest: false,
         notExchange: true,
         InvestLimit: false,
-        hasExchange: true,
-        actEnding: false
+        hasExchange: false,
+        actEnding: 1,
+        showMask: false,
+        upperLimit: true,
+        receiveBox: false
       }
     },
     created: function () {
+      this.getAward()
+      if (!this.token) {
+        alert('请先登录！')
+      }
     },
     props: ['token'],
     methods: {
+      ShowReceive: function () {
+        this.showMask = !this.showMask
+        this.showMask ? ModalHelper.afterOpen() : ModalHelper.beforeClose()
+      },
       getUserAuth: function () {
-        this.$http({
+        var that = this
+        that.$http({
           methods: 'get',
-          url: '/hongcai/rest/users/0/userAuth?token=' + this.token
-        }).then((response) => {
-          this.userAuth = response.data
-          console.log(this.userAuth.active)
-          console.log(this.userAuth.authStatus)
+          url: '/hongcai/rest/users/0/userAuth?token=' + that.token
+        }).then(function (response) {
+          if (response && response.ret !== -1) {
+            that.userAuth = response.data
+            console.log(that.userAuth.active)
+            console.log(that.userAuth.authStatus)
+          }
+        })
+      },
+      getAward: function () {
+        var that = this
+        that.$http({
+          methods: 'get',
+          url: '/hongcai/rest/activitys/mango/award'
+        }).then(function (response) {
+          if (response && response.ret !== -1) {
+            that.Member = response.data.list
+            that.monthCount = response.data.monthCount
+            // that.actEnding = response.data.activityStatus
+            that.actEnding = 2
+          }
+        })
+      },
+      getReward: function () {
+        var that = this
+        that.$http({
+          methods: 'get',
+          url: '/hongcai/rest/users/0/userAuth?token=' + that.token
+        }).then(function (response) {
+          if (response && response.ret !== -1) {
+          }
         })
       },
       toMessage: function () {
-        bridgeUtil.webConnectNative('HCNative_CheckUserAuth', '', {}, function (response) {}, null)
+        this.ShowReceive()
+        bridgeUtil.webConnectNative('HCNative_GoMessage', null, {}, function (response) {}, null)
+      },
+      toCheckAuth: function () {
+        this.ShowReceive()
+        if (this.userAuth.active && this.userAuth.authStatus === 2) {
+          this.toMessage()
+        } else {
+          bridgeUtil.webConnectNative('HCNative_CheckUserAuth', null, {}, function (response) {}, null)
+        }
+      },
+      toExchange: function () {
+        if (this.noInvest) {
+          bridgeUtil.webConnectNative('HCNative_GoInvestList', null, {}, function (response) {}, null)
+        } else if (this.notExchange && this.hasExchange) {
+          this.toMessage()
+        } else {
+          this.getReward()
+        }
       }
     }
   }
@@ -162,15 +234,20 @@
   .padding-b-5 {
     padding-bottom: .5rem;
   }
-  .margin-t-1 {
-    margin-top: .5rem !important;
+  .margin-b-1 {
+    margin-bottom: .35rem;
+  }
+  .margin-b-0 {
+    margin-bottom: 0 !important;
   }
   .Mango {
     background: #ff4b64;
-    
+    background: url('../../images/mangoTV/bg.png') no-repeat 0 0;
+    background-size: 100% 100%;
+    overflow: hidden;
     font-family: PingFang-SC;
   }
-  .boxes {
+  .courtesies {
     padding: 0 .3rem 0 .15rem;
   }
   .head-img {
@@ -186,7 +263,7 @@
     bottom: 1.55rem;
     left: 30%;
     height: .3rem;
-    line-height: .4rem;
+    line-height: .36rem;
     font-size: .25rem;
     font-weight: 500;
     letter-spacing: -1.2px;
@@ -229,31 +306,31 @@
   }
   .courtesy1-lf {
     width: 45%;
-        margin-left: .1rem;
   }
   .courtesy1-rt {
-    width: 52%;
+    width: 50%;
     float: right;
   }
   .courtesy1-lf .pic {
-    width: 2rem;
     height: 1.5rem;
     border-radius: 5px;
     background-color: #5ba4f1;
-    
+    background: url('../../images/mangoTV/courtesy1-vip.png') no-repeat center center;
+    background-size: contain;
   }
   .stock {
-    width: 1.5rem;
+    width: 1.8rem;
     height: .4rem;
-    border-radius: 9.2px;
+    border-radius: .5rem;
     /* background-image: linear-gradient(277deg, #2af8b3, #fee97a); */
-    border-style: solid;
+    /* border-style: solid;
     border-width: 2px;
     border-image-source: linear-gradient(277deg, #2af8b3, #fee97a);
-    border-image-slice: 1;
+    border-image-slice: 1; */
+    border: 1px solid #2af8b3;
     font-size: .21rem;
     font-weight: bold;
-    line-height: 1.64;
+    line-height: .4rem;
     letter-spacing: -0.3px;
     color: #8e8c8c;
     margin-top: .15rem;
@@ -267,24 +344,22 @@
     letter-spacing: 0.7px;
     text-align: justify;
     color: #333333;
-    margin-bottom: .3rem;
   }
   .txt .ft-org {
     color: #ff5500;
   }
   .InvestBtn {
-    width: 2.4rem;
-    height: .7rem;
-    line-height: .7rem;
-    border-radius: 23.2px;
-    background-color: #ffde01;
-    border: solid 1px #b65803;
-    font-size: .23rem;
+    width: 100%;
+    height: .93rem;
+    line-height: .95rem;
+    font-size: .22rem;
     font-weight: bold;
     letter-spacing: 0.7px;
     text-align: center;
     color: #fd6200;
     margin: 0 auto;
+    background: url('../../images/mangoTV/change-btn.png') no-repeat center center;
+    background-size: contain;
   }
   .courtesy2-content {
     margin: 0 0.02rem 0 .17rem;
@@ -298,6 +373,7 @@
   }
   .courtesy2-card li {
     width: 48%;
+    margin-bottom: .3rem;
   }
   .actEnd {
     position: absolute;
@@ -312,13 +388,6 @@
     letter-spacing: -0.4px;
     color: #666666;
   }
-  .card-level {
-    font-size: .35rem;
-    font-weight: bold;
-    line-height: 1.44;
-    letter-spacing: -0.6px;
-    color: #ffffff;
-  }
   .courtesy2-card li:nth-child(odd) {
     float: left;
   }
@@ -326,13 +395,28 @@
     float: right;
   }
   .courtesy2-card li .card {
-    background: #ffde01;
     border-radius: .2rem;
-    padding: .3rem 0;
-    margin-top: .35rem;
+    padding-top: 1.33rem;
+    height: 1.8rem;
+  }
+  .courtesy2-card li:nth-child(1) .card {
+    background: url('../../images/mangoTV/vip1.png') no-repeat center center;
+    background-size: contain;
+  }
+  .courtesy2-card li:nth-child(2) .card {
+    background: url('../../images/mangoTV/vip2.png') no-repeat center center;
+    background-size: contain;
+  }
+  .courtesy2-card li:nth-child(3) .card {
+    background: url('../../images/mangoTV/vip3.png') no-repeat center center;
+    background-size: contain;
+  }
+  .courtesy2-card li:nth-child(4) .card {
+    background: url('../../images/mangoTV/vip4.png') no-repeat center center;
+    background-size: contain;
   }
   .reward {
-    padding: 0.5rem .25rem;
+    padding: 0.3rem 0.1rem .25rem;
     text-align: center;
   }
   .noInvest {
@@ -343,7 +427,7 @@
     box-shadow: 0 2px 3.5px 0 rgba(167, 76, 36, 0.74);
     border: solid 1px #ffee5d;
     margin: 0.2rem auto .5rem;
-    padding: .25rem .2rem;
+    padding: .25rem .15rem;
     font-size: .24rem;
     font-weight: 500;
     line-height: 1.6;
@@ -351,16 +435,31 @@
     color: #666666;
   }
   .notExchange {
-    width: 75%;
-    height: 89.5px;
-    background-color: #ffffff;
-    box-shadow: 0 2px 3.5px 0 rgba(167, 76, 36, 0.74);
-    border: solid 1px #ffee5d;
+    height: 2.2rem;
     margin: 0.2rem auto .5rem;
+    position: relative;
+  }
+  .reward-1 {
+    background: url('../../images/mangoTV/reward-1.png') no-repeat center center;
+    background-size: contain;
+  }
+  .reward-2 {
+    background: url('../../images/mangoTV/reward-2.png') no-repeat center center;
+    background-size: contain;
+  }
+  .reward-3 {
+    background: url('../../images/mangoTV/reward-3.png') no-repeat center center;
+    background-size: contain;
+  }
+  .reward-4 {
+    background: url('../../images/mangoTV/reward-4.png') no-repeat center center;
+    background-size: contain;
   }
   .exchange {
-    width: 21%;
+    width: 35%;
     position: absolute;
+    right: -10%;
+    top: -18%;
   }
   .tips-red {
     font-weight: bold;
@@ -369,7 +468,6 @@
   .hot-tips {
     color: #ff4b64;
     text-align: justify;
-    margin-top: .2rem;
   }
   .ruleBox {
     text-align: justify;
@@ -414,13 +512,18 @@
 	  background-color: #fab281;
   }
   .successBg {
-    background: url('../../images/mangoTV/act-successBox.png') no-repeat center center;
+    background: url('../../images/mangoTV/mask-bg.png') no-repeat center center;
     background-size: contain;
     margin: 1.5rem auto;
     text-align: center;
-    padding: 2rem .6rem;
+    padding: 1.8rem .6rem;
     width: 5.72rem;
     height: 6rem;
+  }
+  .receive, .upperLimit {
+    background: #fff;
+    border-radius: .1rem;
+    padding: .3rem 0;
   }
   .receive-title {
     width: 75%;
@@ -447,12 +550,11 @@
     letter-spacing: -0.4px;
     text-align: center;
     color: #fd6200;
-    margin: 0 auto;
+    margin: 0.3rem auto 0;
   }
   .btns {
     overflow: hidden;
     clear: both;
-    margin-top: .7rem;
   }
   .comm-bg {
     box-shadow: 0px 2.5px 0 0 rgba(4, 122, 87, 0.55);
