6f7b955b3817fef8ff4d4f16be28372342ec0428
yuyang <yuyang1@hooali.com>
2017-09-11 14:57:41 +0800

    fix(qrcode):二维码页面修改

--- src/components/QRcode.vue
+++ src/components/QRcode.vue
@@ -1,44 +1,83 @@
-<!-- <template>
+<template>
   <div>
     <div id="qrcode"></div>
-    <div class='change'>切换</div>
+    <div class='change' @click="change">切换二维码</div>
   </div>
 </template>
 
 <script>
-  import {QRCode} from '../service/QRcode.js'
+  import {QRCode} from '../service/qrcode.js'
   export default {
-    name: 'managementTeam',
+    name: 'QRcode',
+    data () {
+      return {
+        url: '',
+        qrcode: Object
+      }
+    },
+    watch: {
+      url: function (val) {
+        if (val !== '') {
+        }
+      }
+    },
     mounted () {
-      var qrcode = new QRCode(document.getElementById('qrcode'), {
-        text: 'http://jindo.dev.naver.com/collie',
+      var that = this
+      that.qrcode = new QRCode(document.getElementById('qrcode'), {
+        text: that.url,
         width: 200,
         height: 200,
         colorDark: '#000000',
         colorLight: '#ffffff'
       })
-      qrcode.makeCode('http://m.test321.hongcai.com/activity/channel?act=22&f=oficeweb')
-      document.querySelector('.change').onclick = function () {
-        qrcode.makeCode('http://m.test321.hongcai.com/login')
+    },
+    created () {
+      this.change()
+    },
+    methods: {
+      change () {
+        this.getQrcodeUrl()
+      },
+      getQrcodeUrl () {
+        var that = this
+        that.$http({
+          methods: 'get',
+          url: 'hongcai/rest/users/offline/code'
+        }).then(function (response) {
+          if (response && response.data.ret !== -1) {
+            that.url = process.env.vue_domain + '/activity/register?act=33&code=' + response.data.code
+            that.qrcode.makeCode(that.url + Math.random())
+          }
+        })
       }
     }
   }
-</script> -->
+</script>
 <style scoped>
-  .change {
+  #qrcode {
     margin: 1.5rem auto;
     padding: 0rem 1.4rem;
   }
+  .change {
+    width: 30%;
+    margin: 0 auto;
+    background: orange;
+    height: .6rem;
+    line-height: .65rem;
+    border-radius: .5rem;
+    color: #fff;
+    font-size: .3rem;
+  }
 </style>
 
-<template>
+<!-- <template>
   <div>
     <qrcode-vue :value="value" :size="size" level="H"></qrcode-vue>
     <div class='change'>切换</div>
   </div>
 </template>
 <script>
-  // import QrcodeVue from 'qrcode.vue'
+  import QrcodeVue from 'qrcode.vue'
   export default {
     data () {
       return {
@@ -47,7 +86,7 @@
       }
     },
     components: {
-      // QrcodeVue
+      QrcodeVue
     }
   }
-</script>
+</script> -->
