b56ae21cff3871d73434e027677ee901f2180936
fuqiang <fuqiang@hoolai.com>
2017-09-26 11:49:15 +0800

    fix(fastclick): 添加fastclick 尝试解决在iOS系统下双击页面滚动，点击时间300ms延迟问题

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import FastClick from 'fastclick'
+
+if ('addEventListener' in document) {
+  document.addEventListener('DOMContentLoaded', function () {
+    FastClick.attach(document.body)
+  }, false)
+}
 Vue.use(VueAxios, axios)
 
 /* eslint-disable */
