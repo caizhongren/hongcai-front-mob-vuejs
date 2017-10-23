1b4cfa25540130ff38bf8cff9bbabb17599390a6
yuyang <yuyang1@hooali.com>
2017-10-23 19:05:28 +0800

    feat(game-counting):游戏中部分布局

--- src/components/games/game-counting.vue
+++ src/components/games/game-counting.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="gameCounting">
-        拼手速抢红包
+      <div class="rewardMoney">
+        <span>¥</span><span class="money">0</span>
+      </div>
+      <div class="box">
+        <div class="moneyBox">
+          <img src="../../images/singles-day/money-100.png" width="57%">
+        </div>
+      </div>
     </div>
 </template>
 <script>
@@ -24,5 +31,43 @@
   }
 </script>
 <style scoped>
-
+  .gameCounting {
+    background: #fbdc34;
+    padding: 0.5rem 0;
+    margin-top: 1rem; 
+  }
+  .rewardMoney {
+    background: url('../../images/singles-day/reward-box.png') no-repeat center center;
+    background-size: contain;
+    width: 80%;
+    height: 2.2rem;
+    margin: 0 auto;
+    color: #4f0709;
+    padding: .3rem;
+  }
+  .money {
+    font-family: ArialMT;
+    font-size: .32rem;
+  }
+  .rewardMoney span {
+    display: inline-block;
+    float: left;
+  }
+  .rewardMoney span:nth-child(1) {
+    font-size: .24rem;
+  }
+  .box {
+    background: #fbdc34;
+    height: 7.15rem;
+  }
+  .moneyBox {
+    position: fixed;
+    bottom: -.4rem;
+    left: 2.5%;
+    width: 95%;
+    height: 6.6rem;
+    margin: 0 auto;
+    background: url('../../images/singles-day/money-box.png') no-repeat center bottom;
+    background-size: contain;
+  }
 </style>
