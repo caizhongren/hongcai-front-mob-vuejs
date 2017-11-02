64e079b9237381c7bfbc00b1c05dbe074038e8c4
fuqiang <fuqiang@hoolai.com>
2017-11-02 17:55:51 +0800

    feat(breakEggCalculator.vue): 年华投资计算器弹窗

--- src/components/activity/breakEgg.vue
+++ src/components/activity/breakEgg.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="break-egg">
-    <div class="egg-header">
+    {{showCalculator}}
+    <div class="egg-header" @click="showCalculator = true;showMask = true">
       <img src="../../images/break-egg/hander.png" alt="" width="100%">
+    </div>
+    <div class="mask-common" v-show="showMask">
+      <break-Egg-Calculator :closeCalculator="closeCalculator" :showCalculator="showCalculator" v-show="showCalculator"></break-Egg-Calculator>
     </div>
   </div>
 </template>
 <script>
+  import breakEggCalculator from './breakEggCalculator.vue'
+
   export default {
     name: 'breakEgg',
     data () {
       return {
+        showCalculator: false,
+        showMask: false
       }
-    }
+    },
+    methods: {
+      closeCalculator () {
+        this.showMask = false
+        this.showCalculator = false
+      }
+    },
+    components: {breakEggCalculator}
   }
 </script>
 <style scoped>
