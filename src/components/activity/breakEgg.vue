991b1d1ed099a43f3d7ad61426beb11b660c5409
yuyang <yuyang1@hooali.com>
2017-11-02 16:21:29 +0800

    fix(images):双蛋活动图片文件名称修改

--- src/components/activity/breakEgg.vue
+++ src/components/activity/breakEgg.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="break-egg">
-    活动主页
+    <div class="egg-header">
+      <img src="../../images/break-egg/hander.png" alt="" width="100%">
+    </div>
   </div>
 </template>
 <script>
@@ -15,5 +17,8 @@
 <style scoped>
   .break-egg {
     background: #fa6654;
+  }
+  .egg-header {
+
   }
 </style>
