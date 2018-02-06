c3c8c926a31f12ec3a42744c8e80b6cbdd5ea30c
yuyang <yuyang1@hooali.com>
2018-02-06 16:41:08 +0800

    fix(disclosure):信息披露页面

--- src/components/disclosure/businessInformation.vue
+++ src/components/disclosure/businessInformation.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="business">
     <!-- 累计交易总额 -->
-    <div class="cumulativeTotal commonBoxShadow">
+    <div class="commonBoxShadow">
       <p class="title">累计交易总额</p>
       <p class="data">{{cumulative.amount}}元</p>
     </div>
     <!-- 累计交易笔数 -->
-    <div class="cumulativeTotal commonBoxShadow">
+    <div class="commonBoxShadow">
       <p class="title">累计交易笔数</p>
-      <p class="data">{{cumulative.numOfTransactions}}次</p>
+      <p class="data">{{cumulative.numOfTransactions}}笔</p>
+    </div>
+    <!-- 借贷余额 -->
+    <div class="commonBoxShadow">
+      <p class="title">借贷余额</p>
+      <p class="data">{{cumulative.investingTotalLoanAmount}}元</p>
+    </div>
+    <!-- 待还借款笔数 -->
+    <div class="commonBoxShadow">
+      <p class="title">待还借款笔数</p>
+      <p class="data">{{cumulative.investingNumOfTransactions}}笔</p>
     </div>
       <!-- 累计注册会员数 -->
-    <div class="cumulativeTotal commonBoxShadow">
+    <div class="commonBoxShadow">
       <p class="title">累计注册会员数</p>
       <p class="data">{{cumulative.userCount}}人</p>
+    </div>
+    <!-- 累计借款人数 -->
+    <div class="commonBoxShadow">
+      <p class="title">累计借款人数</p>
+      <p class="data">{{cumulative.numOfBorrows}}人</p>
+    </div>
+    <!-- 累计出借人数 -->
+    <div class="commonBoxShadow">
+      <p class="title">累计出借人数</p>
+      <p class="data">{{cumulative.numOfLends}}人</p>
+    </div>
+    <!-- 当前借款人数 -->
+    <div class="commonBoxShadow">
+      <p class="title">当前借款人数</p>
+      <p class="data">{{cumulative.currentNumOfBorrows}}人</p>
+    </div>
+    <!-- 当前出借人数 -->
+    <div class="commonBoxShadow">
+      <p class="title">当前出借人数</p>
+      <p class="data">{{cumulative.currentNumOfLends}}人</p>
     </div>
     <ul class="otherList">
       <li class="commonBoxShadow">
@@ -47,7 +77,7 @@
         <img src="../../images/disclosure/business-icon3.png" class="fr" width="22%">
       </li>
     </ul>
-    <div class="creatTime">数据统计期限：平台成立至{{updateDate}}日24:00</div>
+    <div class="creatTime">数据统计：截至{{updateDate}}日24:00</div>
   </div>
 </template>
 <script>
@@ -70,6 +100,8 @@
       this.getUpdateDate(this.year, this.month)
     },
     methods: {
+      // 占比保留两位小数
+      // 借贷余额：investingTotalLoanAmount，待还借款笔数：investingNumOfTransactions，累计出借人数：numOfLends，累计借款人数：numOfBorrows，当前出借人数：currentNumOfLends，当前借款人数：currentNumOfBorrows，上个月借贷余额：lastMonthTotalLoanAmount，上个月前十大借款人待还金额：sumTenTopLoanBalance，上月最大单一借款人待还金额：topLoanBalance
       getPlatformData () {
         var that = this
         that.$http({
@@ -115,8 +147,6 @@
     border-radius: 9.5px;
     background-color: #ffffff;
     box-shadow: 0 1.5px 8px 0 rgba(80, 77, 75, 0.2);
-  }
-  .cumulativeTotal {
     width: 90%;
     height: 1.5rem;
     margin: 0.3rem auto;
---
+++ src/components/disclosure/policiesRegulations.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="policiesList">
+    政策法规
+  </div>
+</template>
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+    methods: {
+    },
+    mounted () {
+    }
+  }
+</script>
+<style scoped>
+  .policiesList {}
+</style>
