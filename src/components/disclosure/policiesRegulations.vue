6c85adcbc2c42a4abe345c7c76d3ca94f212bc78
yuyang <yuyang1@hooali.com>
2018-02-08 17:26:21 +0800

    fix(policies):新增政策法规7篇文章

--- src/components/disclosure/policiesRegulations.vue
+++ src/components/disclosure/policiesRegulations.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="policiesList">
     <div class="commonBoxShadow" v-for="(item, index) in policiesList" @click="goText(index)">
-      <span></span>
+      <span v-bind:class="{'border-6': index === 1 || index === 2 || index === 3 || index === 6}"></span>
       <p class="title">{{item.title}}</p>
-      <img src="../../images/disclosure/icon-right.png" alt="">
+      <img src="../../images/disclosure/icon-right.png" alt="" v-bind:class="{'margin-6': index === 0 || index === 4 || index === 5}">
     </div>
   </div>
 </template>
@@ -13,28 +13,25 @@
       return {
         policiesList: [
           {
-            title: '网络借贷信息中介机构业务活动管理暂行办法'
+            title: '中华人民共和国电子签名法'
           },
           {
-            title: '中国银监会办公厅关于印发网络借贷资金存管业务指引的通知'
+            title: '最高人民法院关于审理民间借贷案件适用法律若干问题的规定'
           },
           {
-            title: '中国银监会办公厅关于印发网络借贷信息中介机构业务活动信息披露指引的通知'
+            title: '网络借贷信息中介机构业务活动管理暂行办法 '
           },
           {
-            title: '网络借贷信息中介机构业务活动管理暂行办法'
+            title: '中国人民银行等十部委发布《关于促进互联网金融健康发展的指导意见》'
           },
           {
-            title: '网络借贷信息中介机构业务活动管理暂行办法'
+            title: 'P2P网络借贷风险专项整治工作实施方案'
           },
           {
-            title: '中国银监会办公厅关于印发网络借贷资金存管业务指引的通知'
+            title: '网络借贷资金存管业务指引'
           },
           {
-            title: '中国银监会办公厅关于印发网络借贷信息中介机构业务活动信息披露指引的通知'
-          },
-          {
-            title: '网络借贷信息中介机构业务活动管理暂行办法'
+            title: '网络借贷信息中介机构业务活动信息披露指引'
           }
         ]
       }
@@ -63,13 +60,19 @@
     overflow: hidden;
     padding-right: .2rem;
   }
+  .commonBoxShadow span.border-6 {
+    height: 1.2rem;
+  }
+  .commonBoxShadow img.margin-6 {
+  	margin-top: .22rem;
+  }
   .commonBoxShadow span {
     opacity: 0.6;
     background-image: linear-gradient(to top, #fd695b, #fc695b);
     box-shadow: 2px -0.4px 8px 0 rgba(252, 105, 91, 0.53);
     width: .12rem;
     display: inline-block;
-    height: 1.2rem;
+    height: .8rem;
     float: left;
   }
   .commonBoxShadow .title {
