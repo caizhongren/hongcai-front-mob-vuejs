80f6b16260aa6576e31b9c2030582638c34f0cf4
yuyang <yuyang1@hooali.com>
2018-03-27 11:07:46 +0800

    feat(foolday):活动首页活动用户头像、昵称

--- src/components/activity/FoolDays.vue
+++ src/components/activity/FoolDays.vue
@@ -1,21 +1,40 @@
 <template>
   <!-- 活动主页面 -->
-  <router-view></router-view>
+  <router-view :userInfo="userInfo"></router-view>
 </template>
 <script>
   import {Utils} from '../../service/Utils.js'
   export default {
     data () {
       return {
+        wechat_code: this.$route.query.code,
+        userInfo: {
+          nickName: '',
+          headImgUrl: ''
+        }
       }
     },
     props: ['showErrMsg'],
-    watch: {},
+    watch: {
+      wechat_code: function (val) {
+        val !== '' ? this.getUserInfo() : null
+      }
+    },
     mounted () {},
     created: function () {
       this.checkLogin()
+      this.wechat_code !== '' ? this.getUserInfo() : null
     },
     methods: {
+      getUserInfo () {
+        var that = this
+        that.$http('/hongcai/rest/users/' + that.wechat_code + '/openid').then(function (res) {
+          if (res.data && res.data.ret !== -1) {
+            that.userInfo.headImgUrl = res.data.headImgUrl || 'http://test321.hongcai.com/uploads/jpeg/original/2018-03-22/image/73177830c21f4bc682c358cdaaba2ef3-original.jpeg'
+            that.userInfo.nickName = res.data.nickName || 'yy'
+          }
+        })
+      },
       checkLogin () {
         if (Utils.isWeixin()) {
           Utils.redirectToWechatAuth(window.location.path)
