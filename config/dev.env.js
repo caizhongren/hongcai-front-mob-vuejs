04f6f5aa5a18fdb8957fdfe8db1660996939071d
zhangfeng <zhangfeng@hoolai.com>
2018-03-27 15:06:42 +0800

    feat(愚人节)：增加微信授权登录操作

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,5 +7,6 @@ module.exports = merge(prodEnv, {
   domain: '"http://m.test321.hongcai.com"',
   WEB_DEFAULT_DOMAIN: '"/hongcai/api/v1"',
   vue_domain: '"http://vue.test321.hongcai.com"',
-  wechatAppid: '"wx02dfe579709d2d95"'
+  wechatAppid: '"wx02dfe579709d2d95"',
+  wechat_redirect_url: '"http://m.test321.hongcai.com/views/get-weixin-code.html"'
 })
--- src/components/activity/FoolDays.vue
+++ src/components/activity/FoolDays.vue
@@ -7,20 +7,46 @@
   export default {
     data () {
       return {
+        wechatCode: ''
       }
     },
     props: ['showErrMsg'],
     watch: {},
     mounted () {},
     created: function () {
+      console.log('sss')
       this.checkLogin()
     },
     methods: {
       checkLogin () {
-        if (Utils.isWeixin()) {
-          Utils.redirectToWechatAuth(window.location.path)
-        } else {
-          console.log('waha')
+        var that = this
+        that.axios({
+          method: 'get',
+          url: '/hongcai/rest/users/checkSession'
+        }).then((response) => {
+          // 已登录，直接返回
+          if (response.data && response.data.ret !== -1) {
+            return
+          }
+          // 未登录，则检查路由中是否存在 code，不存在，则跳转到微信授权
+          if (!that.$route.query.code) {
+            Utils.redirectToWechatAuth(window.location.href)
+          } else {
+            // 存在code，则调用登录接口
+            that.axios({
+              method: 'get',
+              url: '/hongcai/rest/users/' + that.$route.query.code + '/openid'
+            }).then((response) => {
+              if (response.data && response.data.ret !== -1) {
+                console.log('sssaa')
+              } else {
+                Utils.redirectToWechatAuth(window.location.href)
+              }
+            })
+          }
+        })
+        if (!Utils.isWeixin()) {
+          // Utils.redirectToWechatAuth(window.location.href)
         }
       }
     },
