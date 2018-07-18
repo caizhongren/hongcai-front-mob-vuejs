25eb51384560b4e579db234ccb6a7de790a2772e
yuyang <yuyang1@hooali.com>
2018-07-18 11:01:58 +0800

    fix(bean.vue):测试ios商城webview能否获取到token

--- src/components/bean/bean.vue
+++ src/components/bean/bean.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="beans">
+    <div>token:{{token}}</div>
     <transition :name="transitionName">
       <router-view :bean="bean" :baseFileUrl="baseFileUrl"></router-view>
     </transition>
@@ -17,14 +18,13 @@
     watch: {
       'token': function (val) {
         val ? this.getUserPoints() : null
+      },
+      '$route': function (to, from) {
+        this.token ? this.getUserPoints() : null
       }
-      // '$route': function (to, from) {
-      //   this.token ? this.getUserPoints() : null
-      // }
     },
     created () {
-      // this.token ? this.getUserPoints() : null
-      this.$router.push({name: 'BeanDetail'})
+      this.token ? this.getUserPoints() : null
     },
     methods: {
       getUserPoints () {
@@ -32,7 +32,6 @@
         that.$http('/hongcai/rest/users/0/account?token=' + that.token).then(function (res) {
           if (res && res.ret !== -1) {
             that.bean = res.data.points
-            that.$router.push({name: 'BeanRecord'})
           } else {
             console.log(res.data.msg)
           }
