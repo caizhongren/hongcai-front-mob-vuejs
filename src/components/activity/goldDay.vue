d36f02a72657a844529210812a16256b0ad6fe1f
yuyang <yuyang1@hooali.com>
2018-08-06 14:51:23 +0800

    fix(goldDay):活动状态初始值设置修改

--- src/components/activity/goldDay.vue
+++ src/components/activity/goldDay.vue
@@ -84,7 +84,7 @@
     data () {
       return {
         showBtn: false, // 是否显示底部按钮
-        activityStatus: 0, // 0 活动未开始，1 活动进行中，2 活动结束3天内，3 活动结束3天后
+        activityStatus: -1, // 0 活动未开始，1 活动进行中，2 活动结束3天内，3 活动结束3天后
         showMask: false, // 弹窗蒙层
         isCalculator: false, // 年化出借计算器
         isTips: 0, // 0 不显示提示 1 温馨提示 2 活动已结束
